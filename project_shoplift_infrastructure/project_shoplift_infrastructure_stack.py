92de3ad5a89495a810145757028a50aa3b40fdd4
omerhac <omerhac94@gmail.com>
2021-05-04 00:37:38 +0300

    stack with voice recgonition and order processing

--- project_shoplift_infrastructure/project_shoplift_infrastructure_stack.py
+++ project_shoplift_infrastructure/project_shoplift_infrastructure_stack.py
@@ -1,7 +1,10 @@
 from aws_cdk import core as cdk
 from aws_cdk.aws_ec2 import Vpc
-from aws_cdk.aws_ecs_patterns import ApplicationLoadBalancedFargateService, ApplicationLoadBalancedTaskImageOptions, ApplicationLoadBalancedTaskImageOptions
+from aws_cdk.aws_ecs_patterns import ApplicationLoadBalancedFargateService, ApplicationLoadBalancedTaskImageOptions
 from aws_cdk.aws_ecs import ContainerImage, Cluster
+from aws_cdk.aws_apigateway import LambdaRestApi
+import aws_cdk.aws_lambda as _lambda
+
 # For consistency with other languages, `cdk` is the preferred import name for
 # the CDK's core module.  The following line also imports it as `core` for use
 # with examples from the CDK Developer's Guide, which are in the process of
@@ -14,7 +17,7 @@ class ProjectShopliftInfrastructureStack(cdk.Stack):
     def __init__(self, scope: cdk.Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
 
-        vpc = Vpc(self, 'ShopliftVPC', max_azs=3)
+        # setting up voice recognition fargate service
         shoplift_cluster = Cluster(self, 'ShopliftCluster', cluster_name='ShopliftCluster')
         voice_recognition_task = ApplicationLoadBalancedTaskImageOptions(image=ContainerImage.from_registry('tutum/hello-world:latest'),
                                                                          container_port=80)
@@ -26,3 +29,11 @@ class ProjectShopliftInfrastructureStack(cdk.Stack):
                                                                              desired_count=4,
                                                                              listener_port=80,
                                                                              public_load_balancer=True)
+
+        # setting up order processing lambda-api
+        process_order_lambda = _lambda.Function(self, 'ShopliftProcessOrder',
+                                                code=_lambda.Code.asset('lambda'),
+                                                handler='lambda_handler.handler',
+                                                runtime=_lambda.Runtime.PYTHON_3_8)
+        process_order_api = LambdaRestApi(self, 'ShopliftProcessOrderAPI',
+                                          handler=process_order_lambda)
