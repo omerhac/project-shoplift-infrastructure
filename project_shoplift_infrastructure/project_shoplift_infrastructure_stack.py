1cac227c1795c6629143936ee4a60035835e1620
omerhac <omerhac94@gmail.com>
2021-05-04 23:58:37 +0300

    order retrieval lambda can scan orders from ddb

--- project_shoplift_infrastructure/project_shoplift_infrastructure_stack.py
+++ project_shoplift_infrastructure/project_shoplift_infrastructure_stack.py
@@ -42,7 +42,10 @@ class ProjectShopliftInfrastructureStack(cdk.Stack):
         retrieve_orders_lambda = _lambda.Function(self, 'ShopliftRetrieveOrder',
                                                   code=_lambda.Code.asset('lambda'),
                                                   handler='retrieve_orders.handler',
-                                                  runtime=_lambda.Runtime.PYTHON_3_8)
+                                                  runtime=_lambda.Runtime.PYTHON_3_8,
+                                                  environment={
+                                                      'ORDERS_TABLE_NAME': 'ShopliftOrdersTable'
+                                                  })
         retrieve_order_api = LambdaRestApi(self, 'ShopliftRetrieveOrderAPI',
                                           handler=retrieve_orders_lambda)
         orders_table = Table(self, 'ShopliftOrdersTable',
@@ -50,3 +53,4 @@ class ProjectShopliftInfrastructureStack(cdk.Stack):
                              partition_key={
                                  'name': 'order_id', 'type': AttributeType.STRING
                              })
+        orders_table.grant_read_data(retrieve_orders_lambda)  # grant lambda permissions to read from ddb
